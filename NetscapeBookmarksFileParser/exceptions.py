1ed6f185922ea1e8d489fb22328d33294a9a3021
FlyingWolFox <49326973+FlyingWolFox@users.noreply.github.com>
2020-05-09 09:12:22 -0300

    exceptions.py created

---
+++ NetscapeBookmarksFileParser/exceptions.py
@@ -0,0 +1,14 @@
+class TagNotPresent(Exception):
+    pass
+
+
+class EmptyFileException(Exception):
+    pass
+
+
+class RootBookmarksFolderNotFoundException(Exception):
+    pass
+
+
+class ValueEmptyException(Exception):
+    pass
